2787816f9b5fe1a332e47bf59eaca53550f775f9
ivanskypro <kalmych@gmail.com>
2023-03-28 20:49:46 +0300

    Initial commit

---
+++ src/main/java/com/example/telegram_word_bot/TelegramWordBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegram_word_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramWordBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramWordBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegram_word_bot/TelegramWordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_word_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramWordBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
